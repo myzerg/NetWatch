a6c3d9fbcd456e52f88fe7d0ec4bcb1817a2ab8d
myzerg <myzerg@126.com>
2018-07-19 09:15:35 +0800

    first init

---
+++ NetWatch/NetWatch/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Threading;
+
+namespace NetWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("author:myzerg");
+            Console.WriteLine("help：修改config中的如下内容");
+            Console.WriteLine("watch_url ->监视的网址 当前为:" + NetWatch.Properties.Settings.Default.watch_url);
+            Console.WriteLine("wait_second->轮询间隔设定，建议大于1 当前为:" + NetWatch.Properties.Settings.Default.wait_second);
+            Console.WriteLine("Let Begin!");
+            doNetWatch();
+            while (1 == 1) { }
+
+        }
+        static async Task doNetWatch()
+        {
+            int wait_second = NetWatch.Properties.Settings.Default.wait_second;
+            StringCollection url_List = NetWatch.Properties.Settings.Default.watch_url;
+            await Task.Run(() =>
+            {
+                while (true)
+                {
+                    foreach (string url in url_List)
+                    {
+                        string urlreturn = UrlUtily.GetUrltoHtml(url);
+                        Console.WriteLine(urlreturn);
+                        Thread.Sleep(wait_second*1000);
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ NetWatch/NetWatch/UrlUtily.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * 获取指定URL 的HTML内容
+ * author:myzerg@126.com
+ * date:20180701
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetWatch
+{
+    class UrlUtily
+    {
+        public static string GetUrltoHtml(string Url,  string type = "UTF-8")
+        {
+            try
+            {
+                System.Net.HttpWebRequest r = (System.Net.HttpWebRequest)System.Net.HttpWebRequest.Create(Url);
+                r.AllowAutoRedirect = true;
+                r.KeepAlive = false;
+                r.ServicePoint.Expect100Continue = false;
+                r.ServicePoint.UseNagleAlgorithm = false;
+                r.ServicePoint.ConnectionLimit = 65500;
+                r.AllowWriteStreamBuffering = false;
+                r.Proxy = null;
+                System.Net.CookieContainer c = new System.Net.CookieContainer();
+                r.CookieContainer = c;
+                System.Net.HttpWebResponse res = r.GetResponse() as System.Net.HttpWebResponse;
+                System.IO.StreamReader s = new System.IO.StreamReader(res.GetResponseStream(), System.Text.Encoding.GetEncoding(type));
+                string retsult = s.ReadToEnd();
+                res.Close();
+                r.Abort();
+                return retsult;
+
+            }
+            catch (System.Exception ex)
+            {
+                //errorMsg = ex.Message;
+                return ex.Message;
+            }
+        }
+
+    }
+}
